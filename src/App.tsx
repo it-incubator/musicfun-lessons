5f56750d4cd47000acd6b206b9e2895b297bc3d8
dimych <kuzyuberdin@gmail.com>
2025-08-20 22:47:26 +0400

    load track detail

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import {useEffect, useRef, useState} from "react";
 import type {TrackDataItem, TrackResponse, TracksResponse} from "./types.ts";
 
 
+
 export function App() {
     console.log('APP RENDERED')
-    const [isListLoading, setIsListLoading] = useState<boolean>(true)
-    const [isDetailLoading, setIsDetailLoading] = useState<boolean>(false)
-    const [tracks, setTracks] = useState<TrackDataItem[]>([])
-    const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)  // normalization
+    // Status of List of Query
+    const [listQueryStatus, setListQueryStatus] = useState<'success' | 'loading'>('loading') // FSM
+    const [tracks, setTracks] = useState<TrackDataItem[] | null>(null)
 
+    const [detailQueryStatus, setDetailQueryStatus] = useState<'pending' | 'success' | 'loading'>('pending')
     const [selectedTrack, setSelectedTrack] = useState<TrackResponse | null>(null) // denormalization
+
+    const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)  // normalization
     const abortControllerRef = useRef<null | AbortController>(null) // denormalization
 
     useEffect(() => {
@@ -22,13 +25,13 @@ export function App() {
             .then(res => res.json() as Promise<TracksResponse>)
             .then(json => {
                 setTracks(json.data);
-                setIsListLoading(false)
+                setListQueryStatus('success')
             })
     }, [])
 
     const handleSelectTrackClick = (trackId: string) => {
         setSelectedTrackId(trackId);
-        setIsDetailLoading(true);
+        setDetailQueryStatus('loading');
 
         abortControllerRef.current?.abort()
 
@@ -43,7 +46,7 @@ export function App() {
             .then(res => res.json() as Promise<TrackResponse>)
             .then(json => {
                 setSelectedTrack(json);
-                setIsDetailLoading(false);
+                setDetailQueryStatus('success');
             })
 
     }
@@ -55,10 +58,10 @@ export function App() {
                 <ul>
                     <h2>List</h2>
                     {
-                        isListLoading && <p>Loading...</p>
+                        listQueryStatus === 'loading' && <p>Loading...</p>
                     }
 
-                    {tracks.map((track) => {
+                    { listQueryStatus === 'success' && tracks!.map((track) => {
                         const color = track.id === selectedTrackId ? 'red' : 'white'
 
                         return <li style={{color: color}}>
@@ -73,9 +76,9 @@ export function App() {
 
                 <div>
                     <h2>Detail</h2>
-                    {isDetailLoading && <p>Loading...</p>}
+                    {detailQueryStatus === 'loading' && <p>Loading...</p>}
 
-                    {!isDetailLoading && selectedTrack && <div>
+                    {detailQueryStatus === 'success' && selectedTrack && <div>
                         <h3>{selectedTrack.data.attributes.title}</h3>
                         <div>{selectedTrack.data.attributes.addedAt}</div>
                         <div>likes: {selectedTrack.data.attributes.likesCount}</div>
