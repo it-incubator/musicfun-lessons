1b92376f2d76019017edad4a1a0cf5def2ac0830
dimych <kuzyuberdin@gmail.com>
2025-12-10 00:19:41 +0400

    useEffectEvent, useRef, clock, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,23 @@
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter, Route, Routes} from "react-router";
-import {AuthLayout} from "./layouts/AuthLayout.tsx";
-import {CommonLayout} from "./layouts/CommonLayout.tsx";
-import {GlobalLayout} from "./layouts/GlobalLayout.tsx";
-import {LoginPage} from "./pages-layer/LoginPage.tsx";
-import {RegisterPage} from "./pages-layer/RegisterPage.tsx";
-import {NotFoundPage} from "./pages-layer/NotFoundPage.tsx";
-import {ProfilePage} from "./pages-layer/ProfilePage";
-import {TrackDetailPage} from "./pages-layer/TrackDetailPage.tsx";
-import {TracksListPage} from "./pages-layer/TracksListPage.tsx";
+import {useState} from 'react'
+import './App.css'
+import {InfinityListV2} from "./InfinityListV2.tsx";
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: Infinity,
-            gcTime: 60 * 1000
-        }
-    }
-});
-// @ts-expect-error we dont need typing
-window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+function App() {
+  const [count, setCount] = useState(0)
 
-export const App = () => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<GlobalLayout />}>
-                        <Route path={'auth'} element={<AuthLayout />}>
-                            <Route path={'login'} element={<LoginPage />} />
-                            <Route path='register' element={<RegisterPage />} />
-                        </Route>
-                        <Route  element={<CommonLayout />}>
-                            <Route path='/' element={<TracksListPage/>}/>
-                            <Route path='/tracks/:trackId' element={<TrackDetailPage/>}/>
-                            <Route path='/profile/:userId' element={<ProfilePage/>}/>
-                        </Route>
-                        <Route path='*' element={<NotFoundPage />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
-    )
+  return (
+    <>
+        <div>
+            {count < 1 && <InfinityListV2 /> }
+        </div>
+
+        <button onClick={() => setCount(count + 1)}>inc {count}</button>
+        <button onClick={() => setCount(count - 1)}>dec {count}</button>
+        {/*{count < 5 && <SecondsUseEffectEvent />}*/}
+
+    </>
+  )
 }
 
 
+export default App
